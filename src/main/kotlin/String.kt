75e190a8cb58b6b68ec877c65aadc5e9e56d7cb8
helloworld-hkm <81341249+helloworld-hkm@users.noreply.github.com>
2022-12-04 21:10:12 +0700

    tipe data String

---
+++ src/main/kotlin/String.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var firstname:String="Hakim"
+    var lastname:String="Firman"
+    var address: String = """
+        Jalan Soekarno hatta,Gg 5b
+        Rt 12/04 bebel wonokerto
+    """.trimIndent()
+    println(address)
+    println(firstname)
+    println(lastname)
+}
