89546ecb72dd08842f1080a649ecebd6ded0bd17
helloworld-hkm <81341249+helloworld-hkm@users.noreply.github.com>
2022-12-04 17:02:40 +0700

    Initial commit

---
+++ src/main/kotlin/Number.kt
@@ -0,0 +1,16 @@
+fun main() {
+    //  tipe data :integer , floating point number
+//    -integer :Byte,short,int Long
+//    deklarasi
+    var age: Byte =30
+    println(age)
+//    floating point :float ,double
+//    deklarasi
+    var float :Float=8.12F
+    var double:Double=23.2
+    println(float)
+    println(double)
+//    konversi
+    var angka :Int  =age.toInt()
+    println(angka)
+}
